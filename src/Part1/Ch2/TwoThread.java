c002cb3d570e055c44ba051ee16001ed8609fb89
Alexander <berezkin05@gmail.com>
2018-04-26 12:24:31 +0300

    Initial commit

---
+++ src/Part1/Ch2/TwoThread.java
@@ -0,0 +1,21 @@
+package Part1.Ch2;
+
+public class TwoThread extends Thread {
+
+    @Override
+    public void run() {
+
+        for (int i=0; i<100; i++) {
+            System.out.println("New Thread");
+        }
+    }
+
+    public static void main(String[] args) {
+        TwoThread tt = new TwoThread();
+        tt.start();
+
+        for (int i=0; i<100; i++) {
+            System.out.println("Main Thread");
+        }
+    }
+}
