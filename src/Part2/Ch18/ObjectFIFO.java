a49583072aa266050afd9d6385ac0f537231e026
Alexander <berezkin05@gmail.com>
2018-08-20 16:38:35 +0300

    Chapter 18

---
+++ src/Part2/Ch18/ObjectFIFO.java
@@ -0,0 +1,20 @@
+package Part2.Ch18;
+
+public class ObjectFIFO {
+
+    public ObjectFIFO(int numberOfThreads) {
+
+    }
+
+    public Object remove() {
+        return null;
+    }
+
+    public Object[] removeAll() throws InterruptedException{
+        return new Object[0];
+    }
+
+    public void add(Object obj) throws InterruptedException{
+
+    }
+}
