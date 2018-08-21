67e46ab5cbad08b2ba17e7cbd30799a9ef247a99
Alexander <berezkin05@gmail.com>
2018-08-21 16:40:38 +0300

    Chapter 18

--- src/Part2/Ch18/ObjectFIFO.java
+++ src/Part2/Ch18/ObjectFIFO.java
@@ -1,20 +1,129 @@
 package Part2.Ch18;
 
 public class ObjectFIFO {
+    private Object[] queue;
+    private int capacity;
+    private int size;
+    private int head;
+    private int tail;
 
-    public ObjectFIFO(int numberOfThreads) {
-
+    public ObjectFIFO(int cap) {
+        capacity = (cap > 0) ? cap : 1; // at least 1
+        queue = new Object[capacity];
+        head = 0;
+        tail = 0;
+        size = 0;
     }
 
-    public Object remove() {
-        return null;
+    public int getCapacity() {
+        return capacity;
     }
 
-    public Object[] removeAll() throws InterruptedException{
-        return new Object[0];
+    public synchronized int getSize() {
+        return size;
     }
 
-    public void add(Object obj) throws InterruptedException{
+    public synchronized boolean isEmpty() {
+        return (size == 0);
+    }
 
+    public synchronized boolean isFull() {
+        return (size == capacity);
+    }
+
+    public synchronized void add(Object obj) throws InterruptedException{
+        waitWhileFull();
+
+        queue[head] = obj;
+        head = (head + 1) % capacity;
+        size++;
+
+        notifyAll(); // let any waiting threads know about change
+    }
+
+    public synchronized void addEach(Object[] list) throws InterruptedException {
+//        you might want to code more efficient implementation here... (see ByteFIFO.java)
+
+        for (int i = 0; i < list.length; i++) {
+            add(list[i]);
+        }
+    }
+
+    public synchronized Object remove() throws InterruptedException {
+        waitWhileEmpty();
+
+        Object obj = queue[tail];
+
+//        don't block GC by keeping unnecessary reference
+        queue[tail] = null;
+
+        tail = (tail + 1) % capacity;
+        size--;
+
+        notifyAll();
+
+        return obj;
+    }
+
+    public synchronized Object[] removeAll() throws InterruptedException{
+//        you might want to code more efficient implementation here... (see ByteFIFO.java)
+        Object[] list = new Object[size]; // use the current size
+
+        for (int i = 0; i < list.length; i++) {
+            list[i] = remove();
+        }
+
+//        if FIFO was empty, a zero-length array is returned
+        return list;
+    }
+
+    public synchronized Object[] removeAtLeastOne() throws InterruptedException {
+
+        waitWhileEmpty(); // wait for at least one to be in FIFO
+        return removeAll();
+    }
+
+    public synchronized boolean waitUntilEmpty(long msTimeout) throws InterruptedException {
+
+        if (msTimeout == 0L) {
+            waitUntilEmpty();
+            return true;
+        }
+
+//        wait only for the specified amount of time
+        long endTime = System.currentTimeMillis() + msTimeout;
+        long msRemaining = msTimeout;
+
+        while (!isEmpty() && (msRemaining > 0L)) {
+            wait(msRemaining);
+            msRemaining = endTime - System.currentTimeMillis();
+        }
+
+//        may have timed out, or may have met condition, calc return value
+        return isEmpty();
+    }
+
+    public synchronized void waitUntilEmpty() throws InterruptedException {
+        while (!isEmpty()) {
+            wait();
+        }
+    }
+
+    private synchronized void waitWhileEmpty() throws InterruptedException {
+        while (isEmpty()) {
+            wait();
+        }
+    }
+
+    public synchronized void waitUntilFull() throws InterruptedException {
+        while (!isFull()) {
+            wait();
+        }
+    }
+
+    private synchronized void waitWhileFull() throws InterruptedException {
+        while (isFull()) {
+            wait();
+        }
     }
 }
