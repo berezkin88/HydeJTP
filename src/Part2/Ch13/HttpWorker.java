42df777cb96e9744f681ce35373f694db2cabc7b
Alexander <berezkin05@gmail.com>
2018-08-22 15:16:33 +0300

    Chapter 13

---
+++ src/Part2/Ch13/HttpWorker.java
@@ -0,0 +1,17 @@
+package Part2.Ch13;
+
+import Part2.Ch18.ObjectFIFO;
+
+import java.io.File;
+import java.net.Socket;
+
+public class HttpWorker {
+    public HttpWorker(File docRoot, int workerPriority, ObjectFIFO idleWorkers) {
+    }
+
+    public void processRequest(Socket s) {
+    }
+
+    public void stopRequest() {
+    }
+}
