5aedd941381e01f985b2f384df0b3fb8c4db8681
Alexander <berezkin05@gmail.com>
2018-09-11 16:47:29 +0300

    Chapter 18

---
+++ src/Part2/Ch18/ByteFIFO.java
@@ -0,0 +1,28 @@
+package Part2.Ch18;
+
+public class ByteFIFO {
+
+    public ByteFIFO(int bufferSize) {
+
+    }
+
+    public int getCapacity() {
+        return 0;
+    }
+
+    public void add(byte[] addBuf) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public byte[] removeAll() {
+        return new byte[0];
+    }
+
+    public int getSize() {
+        return 0;
+    }
+}
